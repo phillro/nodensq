a0f8434ae4c4abd9d9f9209dc8354b6804c50097
Anthony Doran <aedoran@gmail.com>
2012-10-10 04:38:57 -0400

    can parse response except for timestamp

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-var nsq = require(__dirname +"/lib/nsq");
+var nsq  = require(__dirname +"/lib/nsq"),
+		conn = require(__dirname + "/lib/conn");
 
 
-console.log(nsq);
+var c = conn.createClient();
+
+c.on("connect",function() {
+	c.write("  V2");
+	var command = nsq.subscribe("test","ch","a","b");
+	c.write(command);
+	c.write(nsq.ready(10));
+});
+
+c.on("data", function(buffer) {
+	var a = nsq.unpack_response(buffer);
+	console.log(a);
+	console.log(nsq.decode_message(a[1]));
+});
+
+
