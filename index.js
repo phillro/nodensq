8ed97b3583a21882971f99c8564a4e2203c051c4
Phillip Rosen <phill.rosen@gmail.com>
2012-10-10 15:53:26 -0400

    fixed timestamp parsing

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var nsq  = require(__dirname +"/lib/nsq"),
 
 
 var c = conn.createClient();
+c.connect();
 
 c.on("connect",function() {
 	c.write("  V2");
-	var command = nsq.subscribe("test","ch","a","b");
+	var command = nsq.subscribe("test","nsq_to_file","a","b");
 	c.write(command);
 	c.write(nsq.ready(10));
 });
